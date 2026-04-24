ec418dae2f120c5a4f3dc08baed04104694dd104
Carol <black.lion3766@gmail.com>
2026-04-24 08:10:17 -0300

    Delete Biblioteca directory

--- Biblioteca/src/data/conection.js
+++
@@ -1,9 +0,0 @@
-const mysql = require("mysql2/promise");
-
-const conection = mysql.createPool({
-    host:"localhost",
-    user: "root",
-    password: "",
-    database: "biblioteca"
-});
-module.exports = conection;
